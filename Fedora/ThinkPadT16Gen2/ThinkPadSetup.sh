9b08adb35e82ebeb794433ed670a601db56fc8f4
Rekko12 <106546011+Rekko12@users.noreply.github.com>
2026-05-09 08:38:04 +0200

    Add libimobiledevice to essential applications installation

--- Fedora/ThinkPadT16Gen2/ThinkPadSetup.sh
+++ Fedora/ThinkPadT16Gen2/ThinkPadSetup.sh
@@ -170,7 +170,7 @@ dnf install -y @virtualization
 # App Installation
 # Install essential applications
 color_echo "yellow" "Installing essential applications..."
-dnf install -y btop inxi fastfetch unzip unrar git wget curl man-pages tldr 
+dnf install -y btop inxi fastfetch unzip unrar git wget curl man-pages tldr libimobiledevice
 color_echo "green" "Essential applications installed successfully."
 
 # Install Internet & Communication applications
